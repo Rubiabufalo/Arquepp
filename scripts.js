3ae337c56fa3c2aff2f5e2ac432c444774ab7df7
Rubia <rubiabufalofer@outlook.com>
2022-05-17 11:14:54 -0300

    formulario estilizado

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// let usuarion;
+// let button;
+
+
+// button = document.getElementById("btnEnviar");
+
+
+// button.addEventListener ('click', (e) => {
+//     e.preventDefault();
+
+//     usuarion = document.getElementById("usuarion").value;
+
+//     alert(usuarion);
+// });
+
+// $("#btnEnviar").on('click', (e) => {
+//     e.preventDefault();
+
+//     alert($("#usuarion").val());
+// })
